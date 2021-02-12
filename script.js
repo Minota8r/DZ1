289f25009e4e0a70ae4d32ae7b8ec549c85e93ea
Minota8r <77876251+Minota8r@users.noreply.github.com>
2021-02-12 19:07:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let phrases = [
+  'отправить другу смешную гифку',
+  'посмотреть скидки на авиабилеты',
+  'разобраться, о чём поют рэперы',
+  'Юрий Дудь',
+  'расставить книги на полке по цвету',
+  'читать про зарплаты в Сан-Франциско',
+  'Добавить сюда больше фраз',
+  'Открыть окно и вдохнуть леденящий воздух',
+  'Хоть бы эта фраза здесь появилась'
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+
+
+
+button.addEventListener('click', function () {
+ phrase.textContent = getRandomElement(phrases);
+}); 
